13b0450e1b9de7eea83edf796e1664ca8c68733b
Gustavo Richard <gustavo.ricard@outlook.com.br>
2021-12-03 15:46:33 -0300

    Corrigido nome da variavel de ambiente.

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const configuration = require('../../knexfile')
 require('dotenv').config()
 
-const config = !process.env.DATABASE_URI ? configuration.local : configuration.heroku
+const config = !process.env.DATABASE_URL ? configuration.local : configuration.heroku
 const db = knex(config);
 
 module.exports = db;
